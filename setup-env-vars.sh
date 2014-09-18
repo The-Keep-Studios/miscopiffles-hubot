9112649cdf459577630b933f312b8d31c6e0b2f6
Ian Small as Bitnami <ian.small@thekeepstudios.com>
2014-09-18 19:52:35 +0000

    Fast Deployed:

--- setup-env-vars.sh
+++ setup-env-vars.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 export HUBOT_HIPCHAT_JID="147727_1199622@chat.hipchat.com"
 export HUBOT_HIPCHAT_PASSWORD="pinkyandthebrain"
-bin/hubot --adapter hipchat
+echo now run: bin/hubot --adapter hipchat
